90cc54cfd339479bc8e0d1ef5bc8529babf92eea
FalseEdifer <100363784+FalseEdifer@users.noreply.github.com>
2022-08-16 12:07:56 -0300

    Create Relogio.php

---
+++ Relogio.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script>
+		
+			function relogio(){
+				var data=new Date();
+				var hor=data.getHours();
+				var min=data.getMinutes();
+				var seg=data.getSeconds();
+				
+				if(hor < 10){
+					hor="0"+hor;
+				}
+				if(min < 10){
+					min="0"+min;
+				}
+				if(seg < 10){
+					seg="0"+seg;
+				}
+				
+				var horas=hor + ":" + min + ":" + seg;
+				
+				document.getElementById("rel").value=horas;
+			}
+
+			var timer=setInterval(relogio,1000);
+
+		</script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+    <h1>EX 2 16\08\2022</h1>
+    <input type="text" id="rel">
+        
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
