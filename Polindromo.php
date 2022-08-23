af87b357bcb6f88af637c6448657c1ecaeeb51f7
FalseEdifer <100363784+FalseEdifer@users.noreply.github.com>
2022-08-23 12:22:59 -0300

    Create Polindromo.php

---
+++ Polindromo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+    <script type="text/javascript">
+
+      const texto = prompt("digite a palavra: ");
+      const explicacao = ` Um palíndromo é uma palavra, frase ou qualquer outra sequência de unidades que tenha a
+      \n propriedade de poder ser lida tanto da direita para a esquerda como da esquerda para a direita.`
+      const reverso = texto.split('').reverse().join('');
+      console.log( `${explicacao} \n`);
+      if(texto === reverso) {
+      alert(`A palavra ${texto} é um palíndromo!`);
+      } else {
+      alert(`A palavra ${texto} não é um palíndromo!`);
+      }
+  </script>
+  <a class="btn btn-outline-dark" href="index.php" role="button">Index</a>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
